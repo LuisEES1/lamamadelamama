3ba61ae58d6525a5f94277aefd33eb673e2d38e9
Usuario <Usuario@Luis>
2024-05-07 09:01:18 +0200

    Se añade una clase nueva HolaMundoNew

---
+++ TestGIT/src/edu/medac/git/HolaMundoNew.java
@@ -0,0 +1,10 @@
+package edu.medac.git;
+
+public class HolaMundoNew {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+	}
+
+}
